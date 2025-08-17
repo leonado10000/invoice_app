6406755145b397b1625d4e9467aaff9f696bb4f2
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2025-08-17 20:27:23 +0530

    fix:vercel:logs wsgi

--- invoice_app/wsgi.py
+++ invoice_app/wsgi.py
@@ -11,3 +11,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "invoice_app.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+app = get_wsgi_application()
