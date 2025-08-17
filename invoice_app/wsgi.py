2421946434050b2f5ac609ae7d327787cf23c561
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2025-08-17 20:19:25 +0530

    fix:vercel:logs

--- invoice_app/wsgi.py
+++ invoice_app/wsgi.py
@@ -6,12 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
-import os
-
+import os, sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "invoice_app.settings")
+print("=== RUNTIME DEBUG ===")
+print("Python version:", sys.version)
+print("Sys path:", sys.path)
+print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
+print("=====================")
 
 application = get_wsgi_application()
 app = get_wsgi_application()
