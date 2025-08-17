d0e3a1dfe4e36f4032828d8598952f3a56784fab
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2025-08-17 20:25:53 +0530

    fix:vercel:logs wsgi

--- invoice_app/wsgi.py
+++ invoice_app/wsgi.py
@@ -1,13 +1,4 @@
-"""
-WSGI config for invoice_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
 import os, sys
-from django.core.wsgi import get_wsgi_application
 
 print("=== RUNTIME DEBUG ===")
 print("Python version:", sys.version)
@@ -15,5 +6,8 @@ print("Sys path:", sys.path)
 print("DJANGO_SETTINGS_MODULE:", os.environ.get("DJANGO_SETTINGS_MODULE"))
 print("=====================")
 
+# 👇 Force Django to know where settings.py is
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "invoice_app.settings")
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-app = get_wsgi_application()
